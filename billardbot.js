94c8442cfb4f6e380ba45895f5f43e7d3a35eea1
Tobias Borgert <tobias.borgert@gmail.com>
2020-04-27 18:31:30 +0200

    Added simple ping-pong functionality.

--- billardbot.js
+++ billardbot.js
@@ -2,13 +2,27 @@ const Discord = require('discord.js')
 
 const token = require('./token')
 
+const channel_id = '405048908784336896'
+
 const client = new Discord.Client()
+
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
     // List servers the bot is connected to
     console.log("Servers:")
     for (let [key, value] of client.guilds.cache) {
         console.log("--- "  + key + " " + value.name)
+    }
+});
+
+client.on('message', message => {
+    if (message.channel.id == channel_id) {
+        if (message.content === '!ping') {
+            message.channel.send('Pong.');
+        }
+    }
+    else {
+        console.log("Ignoring message in channel '" + message.channel.name + "' [" + message.channel.id + "].")
     }
 });
 
